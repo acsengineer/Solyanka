1653d6cac1e729cc36c49e67901f9da071d1a35d
acsengineer <83394210+acsengineer@users.noreply.github.com>
2023-01-26 15:01:31 +0500

    модуль простого логирования для C#
    модуль простого логирования для C#

---
+++ Log/Log.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace csKrugNStSpkv
+{
+    class Log
+    {
+        private static object locker = new object();
+        private static string logFile;
+        public static void W(string logText, string _logFile="") //W==Write
+        {
+            if (_logFile!="") { logFile = _logFile; }
+            lock (locker)
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter (logFile,true,System.Text.Encoding.Unicode))
+                    {
+                        sw.WriteLine(DateTime.Now.ToString()+" "+logText);
+                    }
+                }
+                catch (Exception)
+                {
+                    ;
+                }
+            }
+
+        }
+
+    }
+}
